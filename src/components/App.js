a51c9b1e9a9d516059d6cf292285dbe81391bc6f
Faizurcse <mdfaizurrahman2000@gmail.com>
2024-05-13 06:30:04 +0530

    jdnjdnjndsjndfjnjdnfd

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+
+import Calender from "./Calendar";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <Calender />
+    </div>
+  );
+};
 
 export default App;
